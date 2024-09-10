7453ab49f36bd755e4b596e9e08b7b0d23b37749
XavierMerino01 <merinoxevi@gmail.com>
2024-09-10 18:29:34 +0200

    Base functionalities

---
+++ javascript.js
@@ -0,0 +1,42 @@
+
+
+let gridParent = document.querySelector(".container");
+let inputBox = document.querySelector(".sizeInput");
+
+CreateStartingGrid(16);
+
+function CreateStartingGrid(gridSize) {
+
+    let totalDivs = gridSize ** 2;
+    let sizePer = 100 / gridSize;
+
+    for (let i = 0; i < totalDivs; i++) {
+
+        let newElement = document.createElement("div");
+        newElement.classList.add("gridElement");
+        newElement.style.height = `${sizePer}%`;
+        newElement.style.width = `${sizePer}%`;
+        newElement.addEventListener('mouseover', () => { newElement.style.backgroundColor = "red"; });
+        gridParent.appendChild(newElement)
+
+    }
+}
+
+inputBox.addEventListener('change', () => ChangeGridSize(inputBox.value));
+
+function ChangeGridSize(value) {
+
+    if (value > 100) {
+        alert("Number has to be lower than 100.");
+        inputBox.value = '';
+        return;
+    }
+
+    inputBox.value = '';
+    while (gridParent.lastElementChild) {
+        gridParent.removeChild(gridParent.lastElementChild);
+    }
+
+    CreateStartingGrid(value);
+}
+
